a8bf640a4adc5e15e1d67becafca4630d19077f0
Your Name <you@example.com>
2022-09-17 23:37:52 +0300

    add task41

--- task41/Program.cs
+++ task41/Program.cs
@@ -2,14 +2,22 @@
 // Посчитайте, сколько чисел больше 0 ввёл пользователь.
 
 Console.WriteLine("Введите числа через пробел и нажмите Enter");
-string numbers = Console.ReadLine()!;
-string[] numbersSplit = numbers.Split(' ');
+string elementsArray = Console.ReadLine();
+string[] elementsSplit = elementsArray.Split(' ');
 
+int[] array = new int[elementsArray.Length];
 
-int[] array = new int[numbersSplit.Length];
-for (int i = 0; i < numbersSplit.Length; i++)
+int rezult=0;
+for (int i = 0; i < elementsSplit.Length; i++)
 {
-    array[i] = int.Parse(numbersSplit[i]);
-    //Console.Write($"{array[i]}{' '}");
+    array[i] = int.Parse(elementsSplit[i]);
+    Console.Write($"{array[i]}{' '}");
+    
+    if (array[i] > 0)
+    {
+        rezult++; 
+    }
 }
-Console.WriteLine();
+Console.WriteLine($"\n Количество положительных чисел = {rezult}");
+
+
