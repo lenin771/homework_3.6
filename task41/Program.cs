ad2f4a8064cb5da57dba364f96650796a411d88c
Your Name <you@example.com>
2022-09-17 20:24:15 +0300

    add task41

---
+++ task41/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. 
+// Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+Console.WriteLine("Введите числа через пробел и нажмите Enter");
+string numbers = Console.ReadLine()!;
+string[] numbersSplit = numbers.Split(' ');
+
+
+int[] array = new int[numbersSplit.Length];
+for (int i = 0; i < numbersSplit.Length; i++)
+{
+    array[i] = int.Parse(numbersSplit[i]);
+    //Console.Write($"{array[i]}{' '}");
+}
+Console.WriteLine();
